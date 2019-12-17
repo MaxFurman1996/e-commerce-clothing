170cdf77cc5725e45a16132e86b26208db72e2f5
MaxFurman <furmanmax96@gmail.com>
2019-12-17 15:51:52 +0200

    implemented redux-persist for cart items, added directory collections and shop items to redux store

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,11 @@
-import React, { Component } from 'react';
-
-import SHOP_DATA from './shop.data';
-import PreviewCollection from '../../components/preview-collection/preview-collection.component';
+import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 
-class ShopPage extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render(){
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({ id, ...collectionProps }) => <PreviewCollection key={id} {...collectionProps} />)
-                }
-            </div>  
-        )
-    }
-}
+const ShopPage = () => (
+    <div className='shop-page'>
+        <CollectionsOverview />
+    </div>  
+);
 
 export default ShopPage;
