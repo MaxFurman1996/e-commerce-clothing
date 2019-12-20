96a673c9cdf64ca0aab043d9ea88934ba8799b08
MaxFurman <furmanmax96@gmail.com>
2019-12-20 10:43:10 +0200

    styled-components used, shop data moved to firebase, added data to redux from firebase, withSpinner HOC added when loading data from firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const firebaseConfig = {
     appId: "1:408350196520:web:dd64c941b43c5bb86b461d"
 };
 
+firebase.initializeApp(firebaseConfig);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
 
@@ -39,8 +41,31 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef;
 }
 
-firebase.initializeApp(firebaseConfig);
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    const batch = firestore.batch();
+    objectsToAdd.forEach(object => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, object);
+    })
+    return await batch.commit();
+}
 
+export const convertCollectionsSnapshotToMap = collections => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    })
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,8 +7,10 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
-    const { title, items } = collection;
+const CollectionPage = (props) => {
+    console.log(props);
+    
+    const { title, items } = props.collection;
     return (
         <div className='collection-page'>
             <h2 className='title'>{title}</h2>
